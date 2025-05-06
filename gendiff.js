9058c1c1f69beb48a6fbe375284b9653951492da
Kate-qp <kpark4328@gmail.com>
2025-05-06 22:38:15 +0300

    Добавлены основные файлы проекта

---
+++ gendiff.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import genDiff from './node_modules/commander/index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+
+program.parse();
